5d81bea9022330e186c71e943393cc43022e18b0
gdlyeabkov <gdlyeabkov@gmail.com>
2021-09-02 18:51:09 +0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -175,6 +175,10 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
 Route::get('{redirectroute}', function ($redirectroute) {
     return redirect('/')->with('redirectroute', Request::path());
 });
